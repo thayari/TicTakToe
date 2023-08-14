01250174b67d65e4d11933485d009bdf46864be9
thayari <50718467+thayari@users.noreply.github.com>
2023-08-14 11:38:18 +0300

    add start screen resize

--- assets/scripts/MainMenuScreen.ts
+++ assets/scripts/MainMenuScreen.ts
@@ -1,4 +1,4 @@
-import { _decorator, Button, Component, Node } from 'cc';
+import { _decorator, Button, math, UITransform, Vec3, view } from 'cc';
 import { Screen } from './Screen';
 import ScreenType from './ScreenType';
 import { GameEventManager } from './GameEvents';
@@ -13,8 +13,29 @@ export class MainMenu extends Screen {
 	@property ({ type: Button })
 	protected startGameButton: Button = null
 
+	protected onLoad(): void {
+		this._resizeLayout()
+	}
+
 	onStartGameButtonClick(): void {
 		GameEventManager.sendSwitchScreen(ScreenType.GameField)
+	}
+
+	private _resizeLayout(): void {
+		const winSize: math.Size = view.getVisibleSizeInPixel()
+		const windowWidth: number = winSize.width
+		const windowHeight: number = winSize.height
+
+		if (windowHeight < windowWidth) {
+			const screenHeight: number = this.node.getComponent(UITransform).height
+
+			if (screenHeight > windowHeight) {
+				const scale = windowHeight / screenHeight
+				const vec3scale = new Vec3(scale, scale, 1)
+
+				this.node.setScale(vec3scale)
+			}
+		}
 	}
 }
 
